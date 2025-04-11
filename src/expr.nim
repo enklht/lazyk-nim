433938c82ac9e12334181f5a773c4298024b0e21
enklht <enklht@gmail.com>
2025-04-12 01:37:49 +1200

    feat: :sparkles: refactor expression handling with improved Atom and Combinator types

--- src/expr.nim
+++ src/expr.nim
@@ -2,22 +2,44 @@ type
   Expr* = ref object
     case isLeaf: bool
     of true:
-      item: Combinator  # Leaf node has no data
+      item: Atom
     of false:
       left, right: Expr
+  Atom = object
+    case isComb: bool
+    of true:
+      comb: Combinator
+    of false:
+      num: int
   Combinator* = enum
-    S, K, I
+    S, K, I,
+    Inc = "<inc>", StdIn = "<stdin>"
 
-func `$`*(tree: Expr): string =
-  if tree.isLeaf:
-    result = $tree.item
-  elif tree.right.isLeaf:
-    result = $tree.left & $tree.right
+func `$`(self: Atom): string =
+  if self.isComb:
+    $self.comb
   else:
-    result = $tree.left & "(" & $tree.right & ")"
+    $self.num
+
+func `$`*(self: Expr): string =
+  if self.isLeaf:
+    result = $self.item
+  elif self.right.isLeaf:
+    result = $self.left & $self.right
+  else:
+    result = $self.left & "(" & $self.right & ")"
+
+func isNum(self: Atom): bool {.inline.} =
+  not self.isComb
+
+func `==`(lhs: Atom, rhs: Combinator): bool =
+  lhs.isComb and lhs.comb == rhs
 
 func leaf*(item: Combinator): Expr =
-  Expr(isLeaf: true, item: item)
+  Expr(isLeaf: true, item: Atom(isComb: true, comb: item))
+
+func num(n: int): Expr =
+  Expr(isLeaf: true, item: Atom(isComb: false, num: n))
 
 func pair*(left, right: Expr): Expr =
   Expr(isLeaf: false, left: left, right: right)
@@ -31,29 +53,71 @@ func pair*(left: Combinator, right: Expr): Expr =
 func pair*(left, right: Combinator): Expr =
   Expr(isLeaf: false, left: leaf(left), right: leaf(right))
 
-func reduceOnce(self: var Expr): bool =
-  if self.isLeaf:
-    return
+func reduce*(self: var Expr)
 
-  if self.left.isLeaf:
-    if self.left.item == I:
-      self = self.right
-      return true
-  elif self.left.left.isLeaf:
-    if self.left.left.item == K:
-      self = self.left.right
-      return true
-  elif self.left.left.left.isLeaf:
-    if self.left.left.left.item == S:
-      let
-        x = self.left.left.right
-        y = self.left.right
-        z = self.right
-      self = pair(pair(x, z), pair(y, z))
-      return true
-  return self.left.reduceOnce() or self.right.reduceOnce()
+func spine(self: var Expr, stack: var seq[Expr]) =
+  while true:
+    stack.add(self)
+    if self.isLeaf:
+      break
+    self = self.left
 
-func reduce*(self: var Expr) =
-  while self.reduceOnce():
-    discard
+func reduce(self: var Expr) =
+  var stack: seq[Expr] = @[]
+  spine(self, stack)
+
+  while true:
+    if stack.len() < 1:
+      break
+    self = stack.pop()
+
+    if self.item.isComb:
+      case self.item.comb
+      of I:
+        if stack.len() < 1:
+          break
+        self = stack.pop().right
+        spine(self, stack)
+      of K:
+        if stack.len() < 2:
+          break
+        self = stack.pop().right
+        discard stack.pop()
+        spine(self, stack)
+      of S:
+        if stack.len() < 3:
+          break
+        let
+          x = stack.pop().right
+          y = stack.pop().right
+          z = stack.pop().right
+        self = pair(pair(x, z), pair(y, z))
+        spine(self, stack)
+      of Inc:
+        if stack.len() < 1:
+          break
+        var n = stack.pop().right
+        n.reduce()
+        if n.isLeaf and n.item.isNum:
+          self = num(n.item.num + 1)
+        else:
+          raise newException(Exception, "cannot increment non number")
+      of StdIn:
+        raise newException(Exception, "todo")
+    else:
+      break
+
+proc run*(self: var Expr): int =
+  self = pair(self, StdIn)
+  while true:
+    var head = pair(pair(pair(self, K), Inc), num(0))
+    head.reduce()
+    if not (head.isLeaf and head.item.isNum):
+      raise newException(Exception, "invalid output format")
+    let n = head.item.num
+    if n >= 256:
+      return n - 256
+    write(stdout, char(n))
+    flushFile(stdout)
+    self = pair(self, pair(K, I))
 
--- src/parse.nim
+++ src/parse.nim
@@ -1,35 +1,39 @@
 import strutils, strformat
 import expr
 
-proc parseOne(input: string): (Expr, string)
-proc parse*(input: string): Expr
+func parseMany(input: string, final: char): (Expr, string)
 
-proc parseOne(input: string): (Expr, string) =
+func parseOne(input: string): (Expr, string) =
   if input.len() == 0:
     raise newException(Exception, "unexpected end of input")
 
   case input[0]
-  of 'S':
+  of 'S', 's':
     (leaf(S), input[1..^1])
-  of 'K':
+  of 'K', 'k':
     (leaf(K), input[1..^1])
-  of 'I':
+  of 'I', 'i':
     (leaf(I), input[1..^1])
   of '(':
-    let posClosingParen = find(input, ')')
-    if posClosingParen == -1:
-      raise newException(Exception, "unmatched delimiter")
-    let expr = parse(input.substr(1, posClosingParen-1))
-    (expr, input.substr(posClosingParen + 1))
+    parseMany(input[1..^1], ')')
+  of ' ', '\t', '\n':
+    parseOne(input[1..^1])
   else:
-    raise newException(Exception, &"unknown character {input[0]}")
+    raise newException(Exception, &"unknown character {input}")
 
-proc parse(input: string): Expr =
+func parseMany(input: string, final: char): (Expr, string) =
   var (current, remaining) = parseOne(input)
 
   while remaining.len() > 0:
+    if remaining[0] == final:
+      return (current, remaining[1..^1])
+
     var next: Expr
     (next, remaining) = parseOne(remaining)
     current = pair(current, next)
 
+  return (current, remaining)
+
+func parse*(input: string): Expr =
+  let (current, remaining) = parseMany(input.strip(), '\0')
   return current
