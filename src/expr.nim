8a6d4c3efb483d2e046c19e097ab48b76d5c0e1f
enklht <enklht@gmail.com>
2025-04-12 12:05:07 +1200

    feat: :sparkles: add isCombOf function and refine reduce logic for clarity

--- src/expr.nim
+++ src/expr.nim
@@ -32,6 +32,9 @@ func `$`*(self: Expr): string =
 func isNum(self: Atom): bool {.inline.} =
   not self.isComb
 
+func isCombOf(self: Expr, other: Combinator): bool {.inline.} =
+  self.item.comb == other
+
 func `==`(lhs: Atom, rhs: Combinator): bool =
   lhs.isComb and lhs.comb == rhs
 
@@ -53,59 +56,44 @@ func pair*(left: Combinator, right: Expr): Expr =
 func pair*(left, right: Combinator): Expr =
   Expr(isLeaf: false, left: leaf(left), right: leaf(right))
 
-func reduce*(self: var Expr)
-
-func spine(self: var Expr, stack: var seq[Expr]) =
-  while true:
-    stack.add(self)
-    if self.isLeaf:
-      break
-    self = self.left
-
-func reduce(self: var Expr) =
+func reduce*(self: var Expr) =
   var stack: seq[Expr] = @[]
-  spine(self, stack)
+  stack.add(self)
 
   while true:
-    if stack.len() < 1:
-      break
-    self = stack.pop()
+    while not stack[^1].isLeaf:
+      stack.add(stack[^1].left)
 
-    if self.item.isComb:
-      case self.item.comb
-      of I:
-        if stack.len() < 1:
-          break
-        self = stack.pop().right
-        spine(self, stack)
-      of K:
-        if stack.len() < 2:
-          break
-        self = stack.pop().right
-        discard stack.pop()
-        spine(self, stack)
-      of S:
-        if stack.len() < 3:
-          break
+    if stack[^1].item.isComb:
+      if stack[^1].isCombOf(I) and stack.len() > 1:
+        stack.setLen(stack.len - 1)
+        stack[^1] = stack[^1].right
+      elif stack[^1].isCombOf(K) and stack.len() > 2:
+        let x = stack[^2].right
+        stack.setLen(stack.len - 2)
+        stack[^1] = x
+      elif stack[^1].isCombOf(S) and stack.len() > 3:
         let
-          x = stack.pop().right
-          y = stack.pop().right
-          z = stack.pop().right
-        self = pair(pair(x, z), pair(y, z))
-        spine(self, stack)
-      of Inc:
-        if stack.len() < 1:
-          break
-        var n = stack.pop().right
+          x = stack[^2].right
+          y = stack[^3].right
+          z = stack[^4].right
+        stack.setLen(stack.len - 3)
+        stack[^1] = pair(pair(x, z), pair(y, z))
+      elif stack[^1].isCombOf(Inc) and stack.len() > 1:
+        stack.setLen(stack.len - 1)
+        var n = stack[^1].right
         n.reduce()
         if n.isLeaf and n.item.isNum:
-          self = num(n.item.num + 1)
+          stack[^1] = num(n.item.num + 1)
         else:
           raise newException(Exception, "cannot increment non number")
-      of StdIn:
+      elif stack[^1].isCombOf(StdIn):
         raise newException(Exception, "todo")
+      else:
+        break
     else:
       break
+  self = stack[^1]
 
 proc run*(self: var Expr): int =
   self = pair(self, StdIn)
