126398f6955877fdd57c5b4693816fdd3cf69153
enklht <enklht@gmail.com>
2025-04-13 18:58:27 +1200

    feat: :sparkles: represent Atom by a pair of numbers

--- src/expr.nim
+++ src/expr.nim
@@ -7,28 +7,40 @@ type
       item: Atom
     of false:
       left, right: Expr
-  AtomKind = enum
-    kComb, kNum, kChar
   Atom = object
-    case kind: AtomKind
-    of kComb:
-      comb: Combinator
-    of kNum:
-      num: uint16
-    of kChar:
-      char: uint16
-  Combinator* = enum
-    S, K, I, V, KI = "Ki",
-    Inc = "<inc>", Read = "<read>",
+    kind: uint8
+    val: uint16
+
+const
+  S*: uint16 = 0
+  K*: uint16 = 1
+  I*: uint16 = 2
+  V*: uint16 = 3
+  Ki*: uint16 = 4
+  Inc*: uint16 = 5
+  Read*: uint16 = 6
+
+const kComb: uint8 = 0
+const kChar: uint8 = 1
+const kNum: uint8 = 2
 
 func `$`(self: Atom): string =
   case self.kind
   of kComb:
-    $self.comb
+    case self.val
+    of S: "S"
+    of K: "K"
+    of I: "I"
+    of V: "V"
+    of Ki: "Ki"
+    of Inc: "<inc>"
+    of Read: "<read>"
+    else: raise newException(Exception, "unreachable")
   of kNum:
-    $self.num
+    $self.val
   of kChar:
-    $self.char
+    $char(self.val)
+  else: raise newException(Exception, "unreachable")
 
 func `$`*(self: Expr): string =
   if self.isLeaf:
@@ -38,28 +50,28 @@ func `$`*(self: Expr): string =
   else:
     result = $self.left & "(" & $self.right & ")"
 
-func leaf*(item: Combinator): Expr =
-  Expr(isLeaf: true, item: Atom(kind: kComb, comb: item))
+func leaf*(item: uint16): Expr =
+  Expr(isLeaf: true, item: Atom(kind: kComb, val: item))
 
 func num(n: uint16): Expr =
-  Expr(isLeaf: true, item: Atom(kind: kNum, num: n))
+  Expr(isLeaf: true, item: Atom(kind: kNum, val: n))
 
 func ch(c: uint16): Expr =
-  Expr(isLeaf: true, item: Atom(kind: kChar, char: c))
+  Expr(isLeaf: true, item: Atom(kind: kChar, val: c))
 
 func pair*(left, right: Expr): Expr =
   Expr(isLeaf: false, left: left, right: right)
 
-func pair*(left: Expr, right: Combinator): Expr =
+func pair*(left: Expr, right: uint16): Expr =
   Expr(isLeaf: false, left: left, right: leaf(right))
 
-func pair*(left: Combinator, right: Expr): Expr =
+func pair*(left: uint16, right: Expr): Expr =
   Expr(isLeaf: false, left: leaf(left), right: right)
 
-func pair*(left, right: Combinator): Expr =
+func pair*(left, right: uint16): Expr =
   Expr(isLeaf: false, left: leaf(left), right: leaf(right))
 
-var stack*: seq[Expr] = @[]
+var stack: seq[Expr] = @[]
 
 proc reduce*(self: Expr): Expr =
   let bottom = stack.len()
@@ -77,56 +89,56 @@ proc reduce*(self: Expr): Expr =
 
     case top.item.kind
     of kComb:
-      if top.item.comb == I and applicable(1):
+      if top.item.val == I and applicable(1):
         stack[^1] = stack[^1].right
-      elif top.item.comb == K and applicable(2):
+      elif top.item.val == K and applicable(2):
         let x = stack[^1].right
         discard stack.pop()
         stack[^1] = x
-      elif top.item.comb == KI and applicable(2):
+      elif top.item.val == KI and applicable(2):
         discard stack.pop()
         let y = stack[^1].right
         stack[^1] = y
-      elif top.item.comb == S and applicable(3):
+      elif top.item.val == S and applicable(3):
         let
           x = stack.pop().right
           y = stack.pop().right
           z = stack[^1].right
         stack[^1].left = pair(x, z)
         stack[^1].right = pair(y, z)
-      elif top.item.comb == V and applicable(3):
+      elif top.item.val == V and applicable(3):
         let
           x = stack.pop().right
           y = stack.pop().right
           f = stack[^1].right
         stack[^1].left = pair(f, x)
         stack[^1].right = y
-      elif top.item.comb == Inc and applicable(1):
+      elif top.item.val == Inc and applicable(1):
         let n = stack[^1].right.reduce()
         if n.isLeaf and n.item.kind == kNum:
-          stack[^1] = num(n.item.num + 1)
+          stack[^1] = num(n.item.val + 1)
         else:
           raise newException(Exception, "cannot increment non number")
-      elif top.item.comb == Read and applicable(1):
+      elif top.item.val == Read and applicable(1):
         let c = readChar(stdin)
         let n: uint16 = if c == '\0': 256 else: uint16(c)
         stack[^1].left[] = pair(pair(leaf(V), ch(n)), leaf(Read))[]
       else:
         break
     of kChar:
-      if applicable(1):
-        let n = top.item.char
+       if applicable(2):
+        let n = top.item.val
         if n == 0:
           discard stack.pop()
           let x = stack[^1].right
           stack[^1] = x
         else:
-          top.item.char -= 1
+          top.item.val -= 1
           let
             f = stack.pop().right
             nfx = stack.pop()
           stack.add(pair(f, nfx))
-    of kNum:
+    else:
       break
 
   while stack.len() > bottom:
@@ -142,9 +154,8 @@ proc run*(self: Expr): int =
     head = head.reduce()
     if not (head.isLeaf and head.item.kind == kNum):
       raise newException(Exception, &"invalid output format: {head}")
-    let n = head.item.num
+    let n = head.item.val
     if n >= 256:
       return int(n - 256)
     write(stdout, char(n))
     current = pair(current, KI)
-
